268d91c1d9858bf20a1b7b0faedb7db1385750d8
Admin <admin@admins-macbook-pro-2.local>
2018-12-20 13:00:27 +0530

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Admin on 20/12/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
